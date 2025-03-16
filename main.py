1db9b9be5d86a772393b88375d0074828d429c05
Murad0998 <134845613+Murad0998@users.noreply.github.com>
2025-03-16 07:20:14 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
+import os
 from telebot.async_telebot import AsyncTeleBot
 from telebot.types import ReplyKeyboardMarkup, KeyboardButton, WebAppInfo
 import asyncio
 
-TOKEN = "7555987714:AAFyWNVqbiTXVOfpMnpKd-aDKfS5HvLWYNk"
+TOKEN = os.getenv("BOT_TOKEN")  # Используем переменные окружения
 bot = AsyncTeleBot(TOKEN)
 
 @bot.message_handler(commands=["start"])
@@ -10,7 +11,7 @@ async def start(message):
     keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
     button = KeyboardButton(
         text="Крутить барабан 🎡",
-        web_app=WebAppInfo(url="https://whatsapp.com/")  # Укажи свой URL
+        web_app=WebAppInfo(url="https://murad0998.github.io/baraban/")
     )
     keyboard.add(button)
 
